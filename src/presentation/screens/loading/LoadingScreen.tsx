363f316e3ba77aeaa6434c52bb20fdc67d819ab8
lucas juarez <lujuarez@emeal.nttdata.com>
2025-08-13 16:19:34 -0300

    add structure folders

---
+++ src/presentation/screens/loading/LoadingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const LoadingScreen = () => {
+  return (
+    <View>
+      <Text>LoadingScreen
+        
+      </Text>
+    </View>
+  )
+}
+
+export default LoadingScreen
