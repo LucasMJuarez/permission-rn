b1d665891311fc9f923da1631b38fad68af891b6
lucas juarez <lujuarez@emeal.nttdata.com>
2025-08-13 16:44:48 -0300

    add navigation structure

---
+++ src/MapsApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { Text } from 'react-native'
+
+const MapsApp = () => {
+  return (
+    <NavigationContainer>
+      <Text> MapSpAP</Text>
+    </NavigationContainer>
+  )
+}
+
+export default MapsApp
